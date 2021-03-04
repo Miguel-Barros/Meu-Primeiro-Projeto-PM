bd6c58bafe53d61288565153c1c7bb6567e5c9f2
Miguel <miguelnatan2@gmail.com>
2021-03-04 19:58:22 -0300

    Update acao.js

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,8 @@
 // This is a JavaScript file
 
+$(document).on("click", "#somar", function(){
+	var valor1 = $("#valor1").val();
+	var valor2 = $("#valor2").val();
+	
+	$("#resultado").val(parseFloat(valor1) + parseFloat(valor2))
+});
