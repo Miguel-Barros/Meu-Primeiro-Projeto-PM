fe052daf73c2e44d2faa400ca7eb2fd37a3d1122
Miguel <miguelnatan2@gmail.com>
2021-03-04 20:13:17 -0300

    Update acao.js

--- www/components/acao.js
+++ www/components/acao.js
@@ -3,6 +3,27 @@
 $(document).on("click", "#somar", function(){
 	var valor1 = $("#valor1").val();
 	var valor2 = $("#valor2").val();
-	
+
 	$("#resultado").val(parseFloat(valor1) + parseFloat(valor2))
+});
+
+$(document).on("click", "#subtrair", function(){
+	var valor1 = $("#valor1").val();
+	var valor2 = $("#valor2").val();
+	
+	$("#resultado").val(parseFloat(valor1) - parseFloat(valor2))
+});
+
+$(document).on("click", "#dividir", function(){
+	var valor1 = $("#valor1").val();
+	var valor2 = $("#valor2").val();
+	
+	$("#resultado").val(parseFloat(valor1) / parseFloat(valor2))
+});
+
+$(document).on("click", "#multiplicar", function(){
+	var valor1 = $("#valor1").val();
+	var valor2 = $("#valor2").val();
+	
+	$("#resultado").val(parseFloat(valor1) * parseFloat(valor2))
 });
